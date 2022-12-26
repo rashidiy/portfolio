77180397685cf602ad83378e1f968e7a0855e143
rashideveloper <bubbless7456@gmail.com>
2022-12-26 09:59:47 +0500

    register models to admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from portfolio.models import Portfolio, Post
+
 
 # Register your models here.
+@admin.register(Portfolio)
+class PortfolioAdmin(ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Post)
+class PortfolioAdmin(ModelAdmin):
+    list_display = ('title',)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from ckeditor.fields import RichTextField
+from django.db.models import Model, CharField, URLField, DateTimeField
+from django_resized import ResizedImageField
+
 
 # Create your models here.
+
+class Portfolio(Model):
+    image = ResizedImageField(size=[560, 560], crop=['middle', 'center'], upload_to='portfolio')
+    title = CharField(max_length=50)
+    short_des = CharField(max_length=50)
+    project_url = URLField('Project Url')
+    created_at = DateTimeField(auto_now_add=True)
+
+
+class Post(Model):
+    image = ResizedImageField(size=[560, 420], crop=['middle', 'center'], upload_to='portfolio')
+    title = CharField(max_length=255)
+    short_des = CharField(max_length=50)
+    long_des = RichTextField()
+    created_at = DateTimeField(auto_now_add=True)
